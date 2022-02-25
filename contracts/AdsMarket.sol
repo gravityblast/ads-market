021f2b6ffd9553b5c5dad54c0647f54be588d1c6
Andrea Franz <andrea@gravityblast.com>
2022-02-25 06:41:47 +0100

    add config methods

--- contracts/AdsMarket.sol
+++ contracts/AdsMarket.sol
@@ -4,11 +4,28 @@ pragma solidity 0.8.10;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AdsMarket is ERC721Enumerable {
+contract AdsMarket is Ownable, ERC721Enumerable {
   using SafeERC20 for IERC20;
 
-  constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+  // tax percentage from 0 to 100_000 (10_000 == 10%)
+  uint256 public taxPerc;
+  // tax duration
+  uint256 public taxPeriod;
+  address public token;
+
+  constructor(string memory _name, string memory _symbol, uint256 _taxPerc, uint256 _taxPeriod, address _token) ERC721(_name, _symbol) {
+    token = _token;
+    setConfig(_taxPerc, _taxPeriod);
   }
+
+  function setConfig(uint256 _taxPerc, uint256 _taxPeriod) public onlyOwner {
+    require(_taxPerc >= 0 && _taxPerc <= 100_000, "taxPerc must be between 0 and 100_000");
+    require(_taxPeriod > 0, "taxPeriod must be greater than 0");
+    taxPerc = _taxPerc;
+    taxPeriod = _taxPeriod;
+  }
+
 }
 
